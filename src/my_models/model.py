888266ddbdecadf96319219163bbab186b736ee6
VladimirMynka <vladimirmynka@yandex.ru>
2023-03-16 03:59:38 +0700

    Structure reorganization
    - Abstract Model class
    - Model-Wrapper class for CLI
    - Save, warmup and find_similar methods, evaluate method

--- src/my_models/model.py
+++ src/my_models/model.py
@@ -1,78 +1,46 @@
-import logging
 from pathlib import Path
 from typing import Optional
 
-import fire
 
-from src.my_models.model import Model
-from src.my_models.model_svd import Model_SVD
-
-logging.basicConfig(filename='example.log', encoding='utf-8')
-
-
-class My_Rec_Model:
-    """
-    Class-wrapper for Model to correct work in CLI
-    """
+class Model:
     def __init__(self):
-        self.model: Model = Model_SVD()
+        pass
 
-    def train(
-        self,
-        train_path: Path = None,
-        save_path: Path = None,
-        **kwargs
-    ) -> None:
+    def train(self, train_path: Path = None, **kwargs) -> None:
         """
-        Receives the dataset filename. Performs model training. Saves the artifacts to ./model/. Logs the results
+        Receives the dataset filename. Performs model training. Saves the artifacts to ./model/. Logs the results.
         :param train_path: path to train dataset
-        :param save_path: path to save weights
         :param kwargs: another params for chosen model
         """
-        self.model.train(train_path, **kwargs)
-        self.save(save_path)
+        pass
 
-    def evaluate(
-        self,
-        data_path: Path,
-        load_path: Optional[Path | str] = None,
-        **kwargs
-    ) -> None:
+    def evaluate(self, data_path: Path, **kwargs) -> None:
         """
         Receives the dataset filename. Loads the model from ./data/model/.
-        Evaluates the model with the provided dataset, prints the results and saves it to the log
+        Evaluates the model with the provided dataset, prints the results and saves it to the log.
         :param data_path: path to evaluating data
-        :param load_path: path to load model
         :param kwargs: another params for chosen model
         """
-        self.warmup(load_path)
-        self.model.evaluate(data_path, **kwargs)
+        pass
 
-    def predict(
-        self,
-        data: list,
-        top_m: int,
-        load_path: Optional[Path | str] = None,
-        **kwargs
-    ) -> list:
+    def predict(self, data: list, top_m: int, **kwargs) -> list:
         """
         Get recommend movies for one user
 
         :param data: list `[[movie_id_1, movie_id_2, .., movie_id_N ], [rating_1, rating_2, .., rating_N]]` for one user
         :param top_m: how much recommended movies must be got
-        :param load_path: path to load model
+        :param kwargs: another params for chosen model
 
         :return: recommended movies with estimated rating in the same as input format
         """
-        self.warmup(load_path)
-        return self.model.predict(data, top_m, **kwargs)
+        pass
 
     def warmup(self, path: Optional[str | Path] = None) -> None:
         """
-        Loads the model from ./data/model/. Refresh if it is already loaded
+        Loads the model from ./data/model/. Refresh if it is already loaded.
         :param path: path for loading model. Default is config['model']
         """
-        self.model.warmup(path)
+        pass
 
     def find_similar(self, movie_id: int, N: int = 5) -> list:
         """
@@ -85,15 +53,11 @@ class My_Rec_Model:
         [movie_name_1, movie_name_2, .., movie_name_N]
         ] Descending sorting by similarity
         """
-        return self.model.find_similar(movie_id, N)
+        pass
 
     def save(self, path: Optional[str | Path] = None) -> None:
         """
         Save model weights to data/model
         :param path: path for saving model. Default is config['model']
         """
-        self.model.save(path)
-
-
-if __name__ == '__main__':
-    fire.Fire(My_Rec_Model)
+        pass
